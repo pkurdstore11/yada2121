5ace707fc1c1e53f1c48d503512d101bd472d535
pkurdstore11 <123010266+pkurdstore11@users.noreply.github.com>
2023-01-22 20:18:29 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  
+	 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7485466725732493" crossorigin="anonymous"></script>
+  
+	<meta charset="UTF-8">
+  <html dir="ltr">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+	<meta http-equiv="Content-Security-Policy" content="default-src * 'self' 'unsafe-inline' 'unsafe-eval' data: gap:">
+	<title>Kurd IOS</title>
+  <meta name="description" content="Apps , Game Hack , Paid Apps Free , *All in the Kurd IOS*">
+  <meta name="language" content="English">
+  <link rel="icon" type="image/png" href="https://is.gd/rIx7VL">
+	<link rel="apple-touch-icon" href="https://is.gd/rIx7VL">
+	<link rel="icon" href="https://is.gd/rIx7VL">
+	<link rel="shortcut icon" href="https://is.gd/rIx7VL">
+	<link rel="apple-touch-startup-image" href="https://is.gd/rIx7VL">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="x/framework7/css/framework7.bundle.min.css">
+    <link rel="stylesheet" href="x/css/icons.css">
+    <link rel="stylesheet" href="x/css/app.css">
+    <link rel="stylesheet" href="x/css/style.css">
+</head>
+
+<body>
